4623c5ba0f99de0d0d1b9d647cf82f7f895e6c54
Benarji Anand <benarji385@gmail.com>
2023-07-05 18:47:29 +0800

    kenzo: Move sepolicy to vendor
    Add the SELinux policy rules to vendor.
    
    Signed-off-by: Benarji Anand <benarji385@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ TARGET_KERNEL_CONFIG := lineageos_kenzo_defconfig
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Sepolicy
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # inherit from the proprietary version
 -include vendor/xiaomi/kenzo/BoardConfigVendor.mk
